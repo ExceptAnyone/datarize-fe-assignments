fbd4941de1e70713099dcc70a82bceadc9b4b46f
장정안 <wjddks96@naver.com>
2025-12-22 17:41:05 +0900

    feat: Provider를 main에 설정

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider, Global } from '@emotion/react';
+import App from './App.tsx';
+import { queryClient } from './lib/react-query/queryClient';
+import { theme } from './styles/theme';
+import { globalStyles } from './styles/globalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Global styles={globalStyles} />
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
